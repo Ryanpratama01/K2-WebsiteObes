13dc489a7b9f9ae4b7632200104d01924711b1af
Iannv <220441100098@student.trunojoyo.ac.id>
2025-04-26 01:20:15 +0700

    coba memasukkan template landing page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/about', function () {
+    return view('landing_page.about');
+
+});
+
+Route::get('/blog', function () {
+    return view('landing_page.blog');
+
+});
+
+Route::get('/contact', function () {
+    return view('landing_page.contact');
+
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing_page.index');
+
+});
+
+Route::get('/models', function () {
+    return view('landing_page.models');
+
 });
