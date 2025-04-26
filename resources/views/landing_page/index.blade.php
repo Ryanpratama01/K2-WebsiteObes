3c8ad074c5573975100e9c96d07b13d07f5ef960
E31232303evimr <158023512+E31232303evimr@users.noreply.github.com>
2025-04-26 22:47:41 +0700

    update navbar dan footer landing page

--- resources/views/landing_page/index.blade.php
+++ resources/views/landing_page/index.blade.php
@@ -5,24 +5,33 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+
       <!-- mobile metas -->
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      
       <!-- site metas -->
-      <title>Modol</title>
+      <title>Obesity Check</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
+
       <!-- bootstrap css -->
       <link rel="stylesheet" type="text/css" href="{{asset('assets/user/css/bootstrap.min.css')}}">
+
       <!-- style css -->
       <link rel="stylesheet" type="text/css" href="{{asset('assets/user/css/style.css')}}">
+
       <!-- Responsive-->
       <link rel="stylesheet" href="{{asset('assets\user\css\responsive.css')}}">
+
       <!-- fevicon -->
-      <link rel="icon" href="{{asset('assets/user/images/fevicon.png')}}" type="image/gif" />
+      <link href="assets/user/images/LogoObes.png" rel="icon">
+      <link href="assets/user/images/logoObes.png" rel="LogoObes">
+
       <!-- Scrollbar Custom CSS -->
       <link rel="stylesheet" href="{{asset('assets\user\css/jquery.mCustomScrollbar.min.css')}}">
+      
       <!-- Tweaks for older IEs-->
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
    </head>
@@ -43,22 +52,12 @@
                         <a class="nav-link" href="about.html">About</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="models.html">Models</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="blog.html">Blog</a>
-                     </li>
-                     <li class="nav-item">
                         <a class="nav-link" href="contact.html">Contact Us</a>
                      </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="information.html">Login</a>
+                     </li>
                   </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                     <!-- Actual search box -->
-                     <div class="form-group has-search">
-                        <input type="text" class="form-control" placeholder="Search">
-                        <span class="fa fa-search form-control-feedback"></span>
-                     </div>
-                  </form>
                </div>
             </nav>
          </div>
@@ -107,23 +106,7 @@
          </div>
       </div>
       <!-- banner section end -->
-      <!-- about section start -->
-      <div class="about_section layout_padding">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="about_taital">About</div>
-                  <p class="about_text">Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,</p>
-                  <div class="read_bt"><a href="#">Read More</a></div>
-               </div>
-               <div class="col-md-6">
-                  <div class="about_img"><img src="{{asset('assets/user/images/about-img.png')}}"></div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- about section end -->
-      <!-- models section start -->
+       <!-- models section start -->
       <div class="models_section layout_padding">
          <div class="container">
             <div class="row">
@@ -206,26 +189,24 @@
          </div>
       </div>
       <!-- models section end -->
-      <!-- blog section start -->
-      <div class="blog_section layout_padding">
-         <div class="blog_section_2">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-md-6 padding_left_0">
-                     <div class="blog_img"><img src="{{asset('assets/user/images/blog-img.png')}}"></div>
-                  </div>
-                  <div class="col-md-6">
-                     <div class="blog_taita_main">
-                        <h3 class="blog_taital_1">alex-hddife</h3>
-                        <p class="ipsum_text">Randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the</p>
-                        <div class="readmore_bt"><a href="#">Read More</a></div>
-                     </div>
-                  </div>
+
+      <!-- about section start -->
+      <div class="about_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="about_taital">About</div>
+                  <p class="about_text">Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,</p>
+                  <div class="read_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-md-6">
+                  <div class="about_img"><img src="{{asset('assets/user/images/about-img.png')}}"></div>
                </div>
             </div>
          </div>
       </div>
-      <!-- blog section end -->
+      <!-- about section end -->
+      
       <!-- contact section start -->
       <div class="contact_section layout_padding">
          <div class="container">
@@ -257,54 +238,44 @@
          </div>
       </div>
       <!-- contact section end -->
-      <!-- footer section start -->
-      <div class="footer_section layout_padding">
-         <div class="container">
-            <div class="footer_main">
-               <div class="location_text">
-                  <ul>
-                     <li>
-                        <a href="#">
-                        <img src="{{asset('assets/user/images/map-icon.png')}}"><span class="padding_left_10">Address : Loram Ipusm</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#">
-                        <img src="{{asset('assets/user/images/call-icon.png')}}"><span class="padding_left_10">Call : +01 1234567890</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#">
-                        <img src="{{asset('assets/user/images/call-icon.png')}}"><span class="padding_left_10">Email : demo@gmail.com</span>
-                        </a>
-                     </li>
-                  </ul>
+
+      <!-- footer section end -->
+      <footer id="footer" class="footer position-relative light-background">
+         <div class="container footer-top">
+            <div class="row gy-4">
+            <div class="col-lg-4 col-md-6 footer-about">
+               <a href="#" class="logo d-flex align-items-center">
+                  <span class="sitename">Mentor</span>
+               </a>
+               <div class="footer-contact pt-3">
+                  <p>Jalan Mastrip, Kec. Sumbersari</p>
+                  <p>Politeknik Negeri Jember</p>
+                  <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
+                  <p><strong>Email:</strong> <span>info@example.com</span></p>
                </div>
-               <div class="quote_bt"><a href="#">Get A Quote</a></div>
+               <div class="social-links d-flex mt-4">
+                  <a href=""><i class="bi bi-twitter-x"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+               </div>
+            </div>
+
+            <div class="col-lg-2 col-md-3 footer-links">
+               <h4>Useful Links</h4>
+               <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About us</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Login</a></li>
+               </ul>
+            </div>
             </div>
          </div>
-      </div>
-      <!-- footer section end -->
-      <!-- copyright section start -->
-      <div class="copyright_section">
-         <div class="container">
-            <p class="copyright_text">2024 All Rights Reserved. Design by <a href="https://html.design">Free Html Templates</a> Distributed by <a href="https://themewagon.com">ThemeWagon</a></p>
-         </div></p>
+
+         <div class="container copyright text-center mt-4">
+            <p>© <span>Copyright</span> <strong class="px-1 sitename">ObesityCheck Team</strong> <span>All Rights Reserved</span></p>
          </div>
-      </div>
-      <!-- copyright section end -->
-      <!-- Javascript files-->
-      <script src="{{asset('assets/user/js/jquery.min.js')}}"></script>
-      <script src="{{asset('assets/user/js/popper.min.js')}}"></script>
-      <script src="{{asset('assets/user/js/bootstrap.bundle.min.js')}}"></script>
-      <script src="{{asset('assets/user/js/jquery-3.0.0.min.js')}}"></script>
-      <script src="{{asset('assets/user/js/plugin.js')}}"></script>
-      <!-- sidebar -->
-      <script src="{{asset('assets/user/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
-      <script src="{{asset('assets/user/js/custom.js')}}"></script>
-      <!-- javascript --> 
-      <script src="{{asset('assets/user/js/owl.carousel.js')}}"></script>
-      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>  
-      <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+      </footer>
    </body>
 </html>
